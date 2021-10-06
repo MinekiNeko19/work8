4f20b56906be1a587eaf657e435f8ee2b9335ecf
Sarah Leong <sleong20@stuy.edu>
2021-10-06 14:14:01 -0400

    added and tested mystrlen

--- strings.c
+++ strings.c
@@ -1,21 +1,31 @@
 #include <stdio.h>
-#include "strings.h";
+#include "strings.h"
 
 int mystrlen(char *s) {
-
-}
-char * mystrncpy( char *dest, char *source ) {
-
-}
-
-char * mystrcat( char *dest, char *source, int n) {
-
-}
-
-int mystrcmp( char *s1, char *s2 ) {
-
+    int len = 0;
+    int i;
+    for (i = 0; i < sizeof(s); i++) {
+        if (*(s+i) != 0) {
+            len++;
+        } else {
+            i = sizeof(s);
+        }
+    }
+    return len;
 }
 
-char * mystrchr( char *s, char c ) {
+// char * mystrncpy( char *dest, char *source ) {
 
-}
+// }
+
+// char * mystrcat( char *dest, char *source, int n) {
+
+// }
+
+// int mystrcmp( char *s1, char *s2 ) {
+
+// }
+
+// char * mystrchr( char *s, char c ) {
+
+// }

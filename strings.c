b16af5b465060c322a517c681db9bf5470a89fac
Sarah Leong <sleong20@stuy.edu>
2021-10-06 11:26:10 -0400

    added strings.c

---
+++ strings.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "strings.h";
+
+int mystrlen(char *s) {
+
+}
+char * mystrncpy( char *dest, char *source ) {
+
+}
+
+char * mystrcat( char *dest, char *source, int n) {
+
+}
+
+int mystrcmp( char *s1, char *s2 ) {
+
+}
+
+char * mystrchr( char *s, char c ) {
+
+}

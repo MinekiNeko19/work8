1cc81d6c12b495fc0af11970fafca625bf34caf0
Sarah Leong <sleong20@stuy.edu>
2021-10-07 15:08:15 -0400

    added strcp

--- strings.c
+++ strings.c
@@ -37,16 +37,17 @@ char * mystrncat( char *dest, char *source, int n) {
 }
 
 int mystrcmp( char *s1, char *s2 ) {
-    int diff = 0;
-    while (*s1 && *s2) {
-        if (*s1 > *s2) {
-            diff = 1;
-        }
-        if (*s1 < *s2) {
-            diff = -1;
-        }
+    while (*s1 == *s2 && *s1 && *s2) {
+        s1++;
+        s2++;
     }
-    return diff;
+    if (*s1 > *s2) {
+        return 1;
+    }
+    if (*s2 > *s1) {
+        return -1;
+    }
+    return 0;
 }
 
 char * mystrchr( char *s, char c ) {
